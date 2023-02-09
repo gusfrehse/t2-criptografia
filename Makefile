32705593a42a4fe7a7dafccb2b27c88fa0172ecc
Gustavo S Frehse <gusfrehse@gmail.com>
2023-02-09 11:54:54 -0300

    encryption and idea

--- Makefile
+++ Makefile
@@ -1,3 +1,4 @@
+CFLAGS += -g
 
 cypher : cypher.o
 
--- cypher.c
+++ cypher.c
@@ -1,16 +1,106 @@
+#include <inttypes.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
-#include <inttypes.h>
+
+#define BUFFER_SIZE 32
+
+#define HEAP_SIZE 1024
+
+struct val {
+    int weight;
+    int chr;
+};
 
 void printhelp(FILE *f, char *exename) {
-    fprintf(f, "Usage:\n\t%s [plaintext]\n\t%s -d [cyphertext] [key]\n", exename, exename);
+    fprintf(f, "Usage:\n\t%s [-d] <key>\n", exename);
 }
 
-void encrypt(char *pt) {
+char getkey(int i, char *key, int keylen) {
+    return key[((i % keylen) + keylen) % keylen];
 }
 
-void decrypt(char *cp, char *key) {
+void insert(struct val* arr, int arrsize, struct val v) {
+    int i;
+    for (i = 0; arr[i].weight <= v.weight && i < arrsize; i++);
+
+    for (int j = arrsize; j > i; j--)
+        arr[j] = arr[j - 1];
+
+    arr[i] = v;
+}
+void printarr(struct val *v, int size) {
+    for (int i = 0; i < size; i++) {
+        printf("%3d ", v[i].weight);
+    }
+    printf("\n");
+    for (int i = 0; i < size; i++) {
+        printf("  %c ", v[i].chr);
+    }
+    printf("\n");
+}
+
+void encrypt(char *key, int keylen) {
+    char buffer[BUFFER_SIZE];
+
+    struct val arr[BUFFER_SIZE];
+    int read;
+
+    while ((read = fread(buffer, 1, BUFFER_SIZE, stdin)) > 0) {
+        //printf("read %d\n", read);
+        for (int i = 0; i < read; i++) {
+            //printarr(arr, i);
+            char k = getkey(i, key, keylen);
+
+            insert(arr, i, (struct val){ .weight = k, .chr = buffer[i] });
+        }
+        //printarr(arr, read);
+
+        for (int i = 0; i < read; i++) {
+            fputc(arr[i].chr, stdout); 
+        }
+    }
+}
+
+void sortkey(char *key, int keylen) {
+    if (keylen <= 1)
+        return;
+
+    printf("key: %s\n", key);
+
+    for (int i = 1; i < keylen; i++) {
+        int j;
+        for (j = i; key[j - 1] >= key[j] && j >= 0; j--) {
+            char tmp = key[j];
+            key[j] = key[j - 1];
+            key[j - 1] = key[j];
+        }
+    }
+
+    printf("key: %s\n", key);
+}
+
+void decrypt(char *key, int keylen) {
+
+    sortkey(key, keylen); 
+    return;
+    char buffer[BUFFER_SIZE];
+
+    struct val arr[BUFFER_SIZE];
+    int read;
+
+    while ((read = fread(buffer, 1, BUFFER_SIZE, stdin)) > 0) {
+        for (int i = 0; i < read; i++) {
+            char k = getkey(i, key, keylen);
+
+            insert(arr, i, (struct val){ .weight = k, .chr = buffer[i] });
+        }
+
+        for (int i = 0; i < read; i++) {
+            fputc(arr[i].chr, stdout); 
+        }
+    }
 }
 
 int main(int argc, char **argv) {
@@ -30,14 +120,13 @@ int main(int argc, char **argv) {
     }
 
     // check if number of arguments is correct
-    if (decryptflag && (optind + 2 != argc) ||
-        !decryptflag && (optind + 1 != argc)) {
+    if (optind + 1 != argc) {
         printhelp(stderr, argv[0]); 
         exit(1);
     }
 
     if (decryptflag)
-        encrypt(argv[optind]);
+        decrypt(argv[optind], strlen(argv[optind]));
     else
-        decrypt(argv[optind], argv[optind + 1]); 
+        encrypt(argv[optind], strlen(argv[optind])); 
 }
